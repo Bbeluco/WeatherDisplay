76994bd406042f247e52d91825d8a6b3cc03f0c9
bbeluco <beluco.bispo@hotmail.com>
2023-09-24 15:40:35 -0300

    Show main city information about weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CityList from './components/cityList/CityList';
 import Header from './components/header/Header';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Header />
       <hr />
+      <CityList />
     </div>
   );
 }
---
+++ src/components/cityList/CityList.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import CSS from 'csstype';
+import getStateCurrentWeatherInfo from "../../requests/axiosRequests";
+
+function CityList(): React.JSX.Element {
+    return (
+        <>
+            <h2 style={style}>Sou a lista de cidades</h2>
+            {
+                getStateCurrentWeatherInfo().map((element, index) => {
+                    return(
+                        <div style={styleColumn}>
+                            <label>{Math.round(element['timelines']['daily'][0]['values']['temperatureMax'])} </label>
+                            <label>{Math.round(element['timelines']['daily'][0]['values']['temperatureMin'])}</label>
+                            <ul style={styleListCapitals}>
+                                <li key={index}>{element['location']['name']}</li>
+                            </ul>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+
+const style: CSS.Properties = {
+    textAlign: 'center'
+}
+
+const styleColumn: CSS.Properties = {
+    columnCount: 3,
+    columnGap: '40px',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+const styleListCapitals: CSS.Properties = {
+    listStyle: 'none'
+}
+export default CityList;
