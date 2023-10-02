c47de3c664905e625209ebbc51c97744964e439a
bbeluco <beluco.bispo@hotmail.com>
2023-10-02 20:55:26 -0300

    Coverage script + test render other element (broken)

--- src/components/seachSpecificWeatherInCity/SeachSpecificWeatherInCity.test.tsx
+++ src/components/seachSpecificWeatherInCity/SeachSpecificWeatherInCity.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import SeachSpecificWeatherInCity from "./SeachSpecificWeatherInCity"
 import '@testing-library/jest-dom'
 
@@ -8,5 +8,16 @@ describe('<SeachSpecificWeatherInCity />', () => {
         expect(screen.getByPlaceholderText('Insira aqui o nome da cidade')).toBeInTheDocument()
         expect(screen.getByRole('button')).toBeInTheDocument()
         expect(screen.queryByTestId('specific_city_info')).not.toBeInTheDocument()
+    })
+
+    it('should render <ResultSpecificCityWeather /> and <FormCityToCheckWeather />', () => {
+        const { rerender }= render(<SeachSpecificWeatherInCity />)
+        const input = screen.getByPlaceholderText('Insira aqui o nome da cidade')
+        fireEvent.change(input, { target: { value: 'Diadema' } })
+        fireEvent.click(screen.getByRole('button'))
+        
+        rerender(<SeachSpecificWeatherInCity />)
+        expect(screen.getByPlaceholderText('Insira aqui o nome da cidade')).toHaveValue('Diadema')
+        expect(screen.getByTestId('specific_city_info')).toBeInTheDocument()
     })
 })
