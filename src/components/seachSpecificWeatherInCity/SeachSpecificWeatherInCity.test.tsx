04c32687bb57c2201b74b75921cc0c568c3ae682
bbeluco <beluco.bispo@hotmail.com>
2023-10-03 16:58:18 -0300

    first mock structure just to check if mock is used

--- src/components/seachSpecificWeatherInCity/SeachSpecificWeatherInCity.test.tsx
+++ src/components/seachSpecificWeatherInCity/SeachSpecificWeatherInCity.test.tsx
@@ -2,6 +2,19 @@ import { fireEvent, render, screen } from "@testing-library/react"
 import SeachSpecificWeatherInCity from "./SeachSpecificWeatherInCity"
 import '@testing-library/jest-dom'
 
+jest.mock("../../requests/axiosRequests", () => {
+
+    function useAxiosRequests() {
+        const requestApiToCheckWeather = () => {
+            console.log('fui chamado')
+        }
+
+        return { requestApiToCheckWeather }
+    }
+
+    return useAxiosRequests
+})
+
 describe('<SeachSpecificWeatherInCity />', () => {
     it('should render only <FormCityToCheckWeather /> component', () => {
         render(<SeachSpecificWeatherInCity />)
@@ -10,13 +23,12 @@ describe('<SeachSpecificWeatherInCity />', () => {
         expect(screen.queryByTestId('specific_city_info')).not.toBeInTheDocument()
     })
 
-    it('should render <ResultSpecificCityWeather /> and <FormCityToCheckWeather />', () => {
-        const { rerender }= render(<SeachSpecificWeatherInCity />)
+    it('should render <ResultSpecificCityWeather /> and <FormCityToCheckWeather />', () => {        
+        render(<SeachSpecificWeatherInCity />)
         const input = screen.getByPlaceholderText('Insira aqui o nome da cidade')
         fireEvent.change(input, { target: { value: 'Diadema' } })
         fireEvent.click(screen.getByRole('button'))
-        
-        rerender(<SeachSpecificWeatherInCity />)
+
         expect(screen.getByPlaceholderText('Insira aqui o nome da cidade')).toHaveValue('Diadema')
         expect(screen.getByTestId('specific_city_info')).toBeInTheDocument()
     })
